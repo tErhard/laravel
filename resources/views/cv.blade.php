3aa74b943fc10baf52c39e60f6142877c479ed4f
telingerErhard <93592012+tErhard@users.noreply.github.com>
2023-03-09 21:43:16 +0200

    Add files via upload

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{{ $telinger }} CV</title>
+</head>
+<body>
+<h1>{{ $telinger }} CV</h1>
+<p>Додайте тут своє CV для посади Junior Laravel Developer</p>
+</body>
+</html>
