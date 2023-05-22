0432d96d0ebd043048611ce96fb06f72fce8c69e
telingerErhard <93592012+tErhard@users.noreply.github.com>
2023-05-22 10:48:57 +0300

    Update StudentController.php

--- modules/module_2/app/Http/Controllers/StudentController.php
+++ modules/module_2/app/Http/Controllers/StudentController.php
@@ -3,26 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+use App\Models\Grade;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index()
+    public function show($id)
     {
-        $students = Student::all();
-        return response()->json($students);
-    }
+        $student = Student::find($id);
 
-    public function update(Request $request, $id)
-    {
-        $student = Student::findOrFail($id);
-
-        // Перевірка та оновлення необхідних полів студента
-        $student->name = $request->input('name');
-        $student->course = $request->input('course');
-        $student->specialty = $request->input('specialty');
-        $student->save();
+        if (!$student) {
+            return response()->json(['message' => 'Student not found'], 404);
+        }
 
         return response()->json($student);
+    }
+}
+
+class GradeController extends Controller
+{
+    public function store(Request $request, $id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json(['message' => 'Student not found'], 404);
+        }
+
+        // Валідація даних $request
+
+        $grade = new Grade;
+        $grade->student_id = $id;
+        $grade->subject = $request->input('subject');
+        $grade->score = $request->input('score');
+        $grade->date = $request->input('date');
+        $grade->save();
+
+        return response()->json($grade, 201);
     }
 }
